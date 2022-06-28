80c9ef083454e85ed28553411645620b8cf0e538
svsarthakjain <vishu2952001@gmail.com>
2022-06-28 14:39:07 +0530

    1 st

---
+++ primefactor/src/primefactor/primefactor.java
@@ -0,0 +1,12 @@
+package primefactor;
+
+public class primefactor {
+	 public static void main(String args[]){  
+		  int i,fact=1;  
+		  int number=6;
+		  for(i=1;i<=number;i++){    
+		      fact=fact*i;    
+		  }    
+		  System.out.println("Factorial of "+number+" is: "+fact);    
+		 }  
+}
