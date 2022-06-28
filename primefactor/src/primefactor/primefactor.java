183ef7b0481c75d130e6ebea0c09701ac39fdb30
SARTHAK  JAIN <63348066+svsarthakjain@users.noreply.github.com>
2022-06-28 14:47:43 +0530

    Update primefactor.java
    2 nd

--- primefactor/src/primefactor/primefactor.java
+++ primefactor/src/primefactor/primefactor.java
@@ -3,7 +3,7 @@ package primefactor;
 public class primefactor {
 	 public static void main(String args[]){  
 		  int i,fact=1;  
-		  int number=6;
+		  int number=10;
 		  for(i=1;i<=number;i++){    
 		      fact=fact*i;    
 		  }    
